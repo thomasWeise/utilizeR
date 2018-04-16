99338fc2ce2a295ae6256c5db310d30a95854067
Thomas Weise <tweise@hfuu.edu.cn>
2018-04-16 15:26:28 +0800

    Added functionOrNULL and numericOrNULL Classes

---
+++ R/classes.R
@@ -0,0 +1,15 @@
+#' @title Either a \code{function} or \code{NULL}
+#' @description A combination of the \code{function} class and \code{NULL}.
+#'   Using this class allows us to have member variables in S4 classes which are
+#'   null-able functions.
+#' @importFrom methods setClassUnion
+#' @exportClass functionOrNULL
+setClassUnion("functionOrNULL", c("function","NULL"))
+
+#' @title Either a \code{numeric} \code{vector} or \code{NULL}
+#' @description A combination of the \code{numeric} class and \code{NULL}. Using
+#'   this class allows us to have member variables in S4 classes which are
+#'   null-able numeric vectors.
+#' @importFrom methods setClassUnion
+#' @exportClass numericOrNULL
+setClassUnion("numericOrNULL", c("numeric","NULL"))
